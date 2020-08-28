808d7065711a53ec5079d0da7b331b99599ac696
twenty5151 <tsuiyikching@protonmail.com>
2020-08-28 21:25:01 +0800

    Add tests for choice, criteria and weights wizard page

--- tests/test_wizard.py
+++ tests/test_wizard.py
@@ -1,56 +1,134 @@
+import pytest
+import numpy as np
 from PySide2.QtCore import Qt
 from unittest.mock import Mock, call
 
 from gui import wizard
 
 
-class SubscriptableMock(Mock):
-    subscripts = []
+def new_subscriptable_mock():
+    class SubscriptableMock(Mock):
+        subscripts = []
+        set_items = []
 
-    def __getitem__(self, other):
-        self.subscripts.append(other)
+        def __getitem__(self, other):
+            self.subscripts.append(other)
+            return self
+
+        def __setitem__(self, item, value):
+            self.subscripts.append(item)
+            self.set_items.append(value)
+            return self
+
+    return SubscriptableMock()
 
 
-def test_choices_wizard_page(qtbot):
-    w = wizard.Wizard()
-    matrix = SubscriptableMock()
-    w.matrix = matrix
-    qtbot.addWidget(w)
-    w.show()
-
-    assert type(w.currentPage()) == wizard.WelcomePage
-
-    qtbot.mouseClick(w.next_button, Qt.LeftButton)
-    assert type(w.currentPage()) == wizard.ChoicesPage
-
-    qtbot.keyClicks(w.currentPage().line_edit, 'apple')
-    assert w.currentPage().line_edit.text() == 'apple'
+def abstract_multi_input_page_tester(qtbot, w, text1, text2):
+    qtbot.keyClicks(w.currentPage().line_edit, text1)
+    assert w.currentPage().line_edit.text() == text1
 
     qtbot.keyClick(w.currentPage().line_edit, Qt.Key_Enter)
     assert w.currentPage().line_edit.text() == ''
-    assert w.currentPage().list.item(0).text() == 'apple'
+    assert w.currentPage().list.item(0).text() == text1
 
-    qtbot.keyClicks(w.currentPage().line_edit, 'orange')
+    qtbot.keyClicks(w.currentPage().line_edit, text2)
     qtbot.mouseClick(w.currentPage().add_button, Qt.LeftButton)
     assert w.currentPage().line_edit.text() == ''
-    assert w.currentPage().list.item(1).text() == 'orange'
+    assert w.currentPage().list.item(1).text() == text2
     assert w.currentPage().list.count() == 2
-
-    assert w.matrix.method_calls == [
-        call.add_choices('apple'),
-        call.add_choices('orange')
-    ]
 
     w.currentPage().list.setCurrentRow(1)
     qtbot.mouseClick(w.currentPage().delete_button, Qt.LeftButton)
     assert w.currentPage().list.count() == 1
-    assert w.currentPage().list.item(0).text() == 'apple'
+    assert w.currentPage().list.item(0).text() == text1
 
-    assert w.matrix.subscripts == [2]
+
+def test_choices_wizard_page(qtbot):
+    w = wizard.Wizard()
+    matrix = new_subscriptable_mock()
+    w.matrix = matrix
+    qtbot.addWidget(w)
+    w.show()
+
+    # Setup
+    qtbot.mouseClick(w.next_button, Qt.LeftButton)
+    assert type(w.currentPage()) == wizard.ChoicesPage
+
+    # Test page
+    abstract_multi_input_page_tester(qtbot, w, 'apple', 'orange')
+
     assert len(w.matrix.method_calls) == 3
-    # 'None' is because subscript mock returned None
-    assert w.matrix.method_calls[-1] == call.df.drop(None, inplace=True)
+    assert w.matrix.method_calls[:-1] == [
+        call.add_choices('apple'),
+        call.add_choices('orange'),
+        #call.df.drop(<SubscriptableMock>, inplace=True),
+    ]
+    assert w.matrix.method_calls[-1][0] == 'df.drop'
+    # The first argument is a <SubscriptableMock> here
+    assert w.matrix.method_calls[-1][2] == {'inplace': True}
 
+
+def test_criteria_wizard_page(qtbot):
+    w = wizard.Wizard()
+    matrix = new_subscriptable_mock()
+    w.matrix = matrix
+    qtbot.addWidget(w)
+    w.show()
+
+    # Setup
+    qtbot.mouseClick(w.next_button, Qt.LeftButton)
+    qtbot.keyClicks(w.currentPage().line_edit, 'apple')
+    qtbot.keyClick(w.currentPage().line_edit, Qt.Key_Enter)
     qtbot.mouseClick(w.next_button, Qt.LeftButton)
     assert type(w.currentPage()) == wizard.CriteriaPage
 
+    # Test page
+    abstract_multi_input_page_tester(qtbot, w, 'taste', 'size')
+
+    assert len(w.matrix.method_calls) == 4
+    assert w.matrix.method_calls[:-1] == [
+        call.add_choices('apple'),
+
+        call.add_criterion('taste', weight=np.nan),
+        call.add_criterion('size', weight=np.nan),
+        #call.df.drop(None, axis='columns', inplace=True),
+    ]
+    assert w.matrix.method_calls[-1][0] == 'df.drop'
+    # The first argument is a <SubscriptableMock> here
+    assert w.matrix.method_calls[-1][2] == {'axis': 'columns', 'inplace': True}
+
+
+def test_weights_wizard_page_basic(qtbot):
+    w = wizard.Wizard()
+    w.page(wizard.Page.Weights).collection = lambda: ['size', 'taste']
+    matrix = new_subscriptable_mock()
+    w.matrix = matrix
+    qtbot.addWidget(w)
+    w.show()
+
+    # Setup
+    qtbot.mouseClick(w.next_button, Qt.LeftButton)
+    qtbot.keyClicks(w.currentPage().line_edit, 'apple')
+    qtbot.keyClick(w.currentPage().line_edit, Qt.Key_Enter)
+    qtbot.mouseClick(w.next_button, Qt.LeftButton)
+    qtbot.keyClicks(w.currentPage().line_edit, 'size')
+    qtbot.keyClick(w.currentPage().line_edit, Qt.Key_Enter)
+    qtbot.keyClicks(w.currentPage().line_edit, 'taste')
+    qtbot.keyClick(w.currentPage().line_edit, Qt.Key_Enter)
+    qtbot.mouseClick(w.next_button, Qt.LeftButton)
+    assert type(w.currentPage()) == wizard.WeightsPage
+
+    # Test page
+    assert len(w.currentPage().sliders) == 2
+    assert len(w.currentPage().spin_boxes) == 2
+
+    # Spin boxes and sliders have synchronized values
+    w.currentPage().spin_boxes[0].setValue(3)
+    assert w.currentPage().spin_boxes[0].value() == w.currentPage().sliders[0].value() == 3
+    w.currentPage().spin_boxes[1].setValue(7)
+    assert w.currentPage().spin_boxes[1].value() == w.currentPage().sliders[1].value() == 7
+
+    # Using up-arrow key to change value
+    qtbot.mouseClick(w.currentPage().spin_boxes[0], Qt.LeftButton)
+    qtbot.keyClick(w.currentPage().spin_boxes[0], Qt.Key_Up)
+    assert w.currentPage().spin_boxes[0].value() == w.currentPage().sliders[0].value() == 4
