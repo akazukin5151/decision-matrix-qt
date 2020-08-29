280c131059fae9fcdf0a93cc9935a5dfca684000
twenty5151 <tsuiyikching@protonmail.com>
2020-08-29 19:36:45 +0800

    Add test_main_add_criteria

--- tests/test_main.py
+++ tests/test_main.py
@@ -33,3 +33,29 @@ def test_main_add_choices(qtbot):
     qtbot.keyClick(ui.lineEdit, Qt.Key_Enter)
     assert ui.matrix_widget.rowCount() == 3
     assert ui.matrix_widget.verticalHeaderItem(2).text() == 'orange'
+
+
+def test_main_add_criteria(qtbot):
+    MainWindow = QMainWindow()
+    ui = main.Ui_MainWindow()
+    qtbot.addWidget(ui)
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+
+    qtbot.mouseClick(ui.combo_box, Qt.LeftButton)
+    qtbot.keyClick(ui.combo_box, Qt.Key_Down)
+    qtbot.keyClick(ui.combo_box, Qt.Key_Enter)
+    assert ui.combo_box.currentText() == 'New criteria'
+
+    qtbot.keyClicks(ui.lineEdit, 'taste')
+    assert ui.lineEdit.text() == 'taste'
+
+    qtbot.mouseClick(ui.pushButton, Qt.LeftButton)
+    assert ui.lineEdit.text() == ''
+    assert ui.matrix_widget.columnCount() == 2
+    assert ui.matrix_widget.horizontalHeaderItem(0).text() == 'taste'
+
+    qtbot.keyClicks(ui.lineEdit, 'color')
+    qtbot.keyClick(ui.lineEdit, Qt.Key_Enter)
+    assert ui.matrix_widget.columnCount() == 3
+    assert ui.matrix_widget.horizontalHeaderItem(1).text() == 'color'
